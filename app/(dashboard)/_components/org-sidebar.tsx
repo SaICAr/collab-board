28b9216b3e69d544c4ff76c7ea3598395bacf912
Monster <1366749348@qq.com>
2024-06-13 17:43:59 +0800

    feat: 搭建dashboard page布局

---
+++ app/(dashboard)/_components/org-sidebar.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export const OrgSidebar = () => {
+  return <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5 bg-red-600">OrgSidebar</div>;
+};
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,3 +1,5 @@
+import { Navbar } from "./_components/navbar";
+import { OrgSidebar } from "./_components/org-sidebar";
 import { Sidebar } from "./_components/sidebar";
 
 interface DashboardLayoutProps {
@@ -8,7 +10,15 @@ const DashboardLayout = ({ children }: DashboardLayoutProps) => {
   return (
     <main className="h-full">
       <Sidebar />
-      <div className="pl-[60px] h-full">{children}</div>
+      <div className="pl-[60px] h-full">
+        <div className="flex gap-x-3 h-full">
+          <OrgSidebar />
+          <div className="h-full flex-1">
+            <Navbar />
+            {children}
+          </div>
+        </div>
+      </div>
     </main>
   );
 };
