d2f97c50fb199a0805fce47db626ac8a7e38a050
Monster <1366749348@qq.com>
2024-06-13 19:45:11 +0800

    chore: 回退clerk版本为@4.31.3解决冲突

---
+++ app/(dashboard)/_components/sidebar/new-button.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { Plus } from "lucide-react";
+import { CreateOrganization } from "@clerk/nextjs";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+
+export const NewButton = () => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <div className="aspect-square">
+          <button className="bg-white/25 h-full w-full rounded-md flex items-center justify-center opacity-60 hover:opacity-100 transition">
+            <Plus className="text-white" />
+          </button>
+        </div>
+      </DialogTrigger>
+      <DialogContent className="p-0 max-w-[480px] bg-transparent border-none">
+        <CreateOrganization />
+      </DialogContent>
+    </Dialog>
+  );
+};
