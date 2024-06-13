bdf764d228bb6dec80e70f06574e853c197f48a5
Monster <1366749348@qq.com>
2024-06-13 21:02:34 +0800

    feat: 实现创建组织功能

--- app/(dashboard)/_components/sidebar/new-button.tsx
+++ app/(dashboard)/_components/sidebar/new-button.tsx
@@ -3,15 +3,18 @@
 import { Plus } from "lucide-react";
 import { CreateOrganization } from "@clerk/nextjs";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { Hint } from "@/components/hint";
 
 export const NewButton = () => {
   return (
     <Dialog>
       <DialogTrigger asChild>
         <div className="aspect-square">
-          <button className="bg-white/25 h-full w-full rounded-md flex items-center justify-center opacity-60 hover:opacity-100 transition">
-            <Plus className="text-white" />
-          </button>
+          <Hint label="Create organization" side="right" align="start" sideOffset={18}>
+            <button className="bg-white/25 h-full w-full rounded-md flex items-center justify-center opacity-60 hover:opacity-100 transition">
+              <Plus className="text-white" />
+            </button>
+          </Hint>
         </div>
       </DialogTrigger>
       <DialogContent className="p-0 max-w-[480px] bg-transparent border-none">
---
+++ components/hint.tsx
@@ -0,0 +1,29 @@
+import { Tooltip, TooltipContent, TooltipTrigger, TooltipProvider } from "@/components/ui/tooltip";
+
+export interface HintProps {
+  label: string;
+  children: React.ReactNode;
+  side?: "top" | "bottom" | "left" | "right";
+  align?: "start" | "center" | "end";
+  sideOffset?: number;
+  alignOffset?: number;
+}
+
+export const Hint = ({ label, children, side, align, sideOffset, alignOffset }: HintProps) => {
+  return (
+    <TooltipProvider>
+      <Tooltip delayDuration={100}>
+        <TooltipTrigger asChild>{children}</TooltipTrigger>
+        <TooltipContent
+          side={side}
+          align={align}
+          sideOffset={sideOffset}
+          alignOffset={alignOffset}
+          className="text-white bg-black border-black"
+        >
+          <p className="font-semibold capitalize">{label}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+};
